05b7463a40715d49b67057cf30b6cbdf1857e5ae
dhbaek2004 <dhbaek2004@gmail.com>
2021-08-27 00:22:26 +0900

    set h2 db, set jdbc, edit build.gradle, application.properties

--- src/main/java/Cherry/spring_example/controller/MemberController.java
+++ src/main/java/Cherry/spring_example/controller/MemberController.java
@@ -5,8 +5,11 @@ import Cherry.spring_example.domain.Member;
 import Cherry.spring_example.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,12 +26,20 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
