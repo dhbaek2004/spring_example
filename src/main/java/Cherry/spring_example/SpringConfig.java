628431d6137cbd11c7834f5e42770109dd76a70f
dhbaek2004 <dhbaek2004@gmail.com>
2021-08-31 00:31:17 +0900

    set h2 db, application.properties, add jdbc template, edit SpringConfig

--- src/main/java/Cherry/spring_example/SpringConfig.java
+++ src/main/java/Cherry/spring_example/SpringConfig.java
@@ -1,13 +1,21 @@
 package Cherry.spring_example;
 
+import Cherry.spring_example.repository.JdbcTemplateMemberRepository;
 import Cherry.spring_example.repository.MemberRepository;
 import Cherry.spring_example.repository.MemoryMemberRepository;
 import Cherry.spring_example.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +23,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
+
+
 }
